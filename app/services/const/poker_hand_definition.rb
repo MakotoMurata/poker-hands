6b24c81aed81cbd07f42bdb3bcd5f23d4caf2558
Makoto Murata <mkt.hollywood.dream@gmail.com>
2020-04-30 16:43:19 +0900

    定数管理のファイル名変更 定数のハッシュ管理

---
+++ app/services/const/poker_hand_definition.rb
@@ -0,0 +1,13 @@
+module HandsModule
+  HANDS = {
+            highcard: "ハイカード",
+            one_pair: "ワンペア",
+            two_pair: "ツーペア",
+            three_cards: "スリーカード",
+            straight: "ストレート",
+            flush: "フラッシュ",
+            full_house: "フルハウス",
+            four_cards: "フォーカード",
+            straight_flush: "ストレートフラッシュ"
+        }
+end
