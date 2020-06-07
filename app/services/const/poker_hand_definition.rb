0f4253c1c9a7d937ceec329a11eaf9575c7b2b7f
Makoto Murata <mkt.hollywood.dream@gmail.com>
2020-06-07 21:10:33 +0900

    定数管理の簡素化

--- app/services/const/poker_hand_definition.rb
+++ app/services/const/poker_hand_definition.rb
@@ -8,6 +8,4 @@ module HandsModule
   FULL_HOUSE = "フルハウス"
   FOUR_CARD = "フォーカード"
   STRAIGHT_FLUSH = "ストレートフラッシュ"
-
-  ALL_HANDS = [HIGH_CARD,ONE_PAIR,TWO_PAIR,THREE_CARD,STRAIGHT,FLUSH,FULL_HOUSE,FOUR_CARD,STRAIGHT_FLUSH]
-  end
+end
